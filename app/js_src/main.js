f8156bae2a262da7fc20811169f1d80d49769323
Dmitry <deimos715@gmail.com>
2025-06-25 12:03:14 +0300

    Initial commit

---
+++ app/js_src/main.js
@@ -0,0 +1,20 @@
+/*
+ * Bootstrap 5
+*/
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+/*
+ * Bootstrap 5 Accardion
+*/
+// import { Collapse } from 'bootstrap';
+
+/*
+ * Bootstrap 5 Alert
+*/
+// import { Alert } from 'bootstrap';
+
+
+/*
+ * My app
+*/
+import './partials/app.js';
